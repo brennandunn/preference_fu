5cac2fd65cd3ea257204d69a2449941851c229cc
Brennan Dunn <me@brennandunn.com>
2009-11-19 11:19:25 -0500

    Initial push at bringing lib up to date.

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -2,13 +2,14 @@ begin
   require File.dirname(__FILE__) + '/../../../../config/environment'
 rescue LoadError
   require 'rubygems'
-  require_gem 'activerecord'
+  require 'active_record'
+  gem 'activerecord'
 end
 
 require 'preference_fu'
 require "#{File.dirname(__FILE__)}/../init"
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :dbfile => ":memory:")
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 
 def setup_db
   ActiveRecord::Schema.define(:version => 1) do
